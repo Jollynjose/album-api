86870c2fddbb6ef0015439b184e426275747e122
jollynjose21 <jollynjose21@gmail.com>
2021-11-08 13:22:10 -0400

    Add config and database modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
+import { Configuration } from 'src/config/config.keys';
+
+import { ConfigModule } from 'src/config/config.module';
+import { ConfigService } from 'src/config/config.service';
+import { DatabaseModule } from 'src/database/database.module';
+import { UserModule } from 'src/modules/user/user.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule, DatabaseModule, UserModule],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  static port: number | string;
+  constructor(private readonly _configService: ConfigService) {
+    AppModule.port = this._configService.get(Configuration.PORT);
+  }
+}
